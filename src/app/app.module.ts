2373dd736bba2c8388a39069d99dfe9238ca1a2e
Gabriela Alves <gabriela_alves1105@hotmail.com>
2023-04-06 15:15:00 -0300

    Editando um herói

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent //esta declarando o componente que faz parte desse modulo
+    AppComponent,
+    HeroesComponent //esta declarando o componente que faz parte desse modulo
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
